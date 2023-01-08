71b641251a96adee1f83738e8752dda34b304ee8
davidomahony <daverock914@gmail.com>
2023-01-08 17:53:54 +0000

    changes from format

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,22 +3,22 @@ data "azurerm_resource_group" "rg-movie-demo" {
 }
 
 resource "azurerm_service_plan" "asp-movie-demo" {
-  name                      = "asp-${local.name}"
-  location                  = "${data.azurerm_resource_group.rg-movie-demo.location}"
-  resource_group_name       = "${data.azurerm_resource_group.rg-movie-demo.name}"
-  os_type                   = "Linux"
-  sku_name                  = "F1"
+  name                = "asp-${local.name}"
+  location            = data.azurerm_resource_group.rg-movie-demo.location
+  resource_group_name = data.azurerm_resource_group.rg-movie-demo.name
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                      = "as-${local.name}"
-  location                  = "${data.azurerm_resource_group.rg-movie-demo.location}"
-  resource_group_name       = "${data.azurerm_resource_group.rg-movie-demo.name}"
-  service_plan_id           = azurerm_service_plan.asp-movie-demo.id
+  name                = "as-${local.name}"
+  location            = data.azurerm_resource_group.rg-movie-demo.location
+  resource_group_name = data.azurerm_resource_group.rg-movie-demo.name
+  service_plan_id     = azurerm_service_plan.asp-movie-demo.id
   site_config {
-    always_on               = false
+    always_on = false
     application_stack {
-      dotnet_version        = "6.0"
+      dotnet_version = "6.0"
     }
   }
 }
