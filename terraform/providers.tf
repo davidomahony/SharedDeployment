a058dd2585adcb941efcb1e583430f7d761deb11
davidomahony <daverock914@gmail.com>
2023-01-10 11:11:34 +0000

    Matching test setup

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,12 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-movie-demo"
+    storage_account_name = "samoviedemo"
+    container_name       = "movie-api-appservices"
+    key                  = "infra-tfstate"
+  }
   required_version = ">=0.14.8"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 locals {
-  name = "test"
+  name = "movie-demo"
 }
